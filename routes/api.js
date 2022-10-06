bad7af04c3b622305a44a065dc63b80b067c852d
xcsmanichi <112955853+xcsmanichi@users.noreply.github.com>
2022-10-06 14:45:23 +0800

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -18,16 +18,19 @@ var ling = require("knights-canvas");
 var googleIt = require("google-it");
 var translate = require("@vitalets/google-translate-api");
 
-var { color, bgcolor } = require(__path + "/lib/color.js");
-var { fetchJson } = require(__path + "/lib/fetcher.js")
-var options = require(__path + "/lib/options.js");
+var { color, bgcolor } = require(__path + '/lib/color.js');
+var { fetchJson } = require(__path + '/lib/fetcher.js')
+var options = require(__path + '/lib/options.js');
+var {
+	Vokal,
+	
+	
+	Base,
+	Searchnabi,
+    Gempa
+} = require('./../lib');
 
-var { igdwn, fbdwn, joox, tiktok, twdwn, styletext, ringtone, quotesAnime, wallpaper, wikimedia, musically, pinterest, dBinary, eBinary, jadwaltv } = require("./../lib/dwn.js");
-var { Vokal, Base, Searchnabi, Gempa } = require("./../lib");
-const { query, text } = require("express");
-const { search } = require("./routes/api.js");
-
-var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
+var { igdwn, fbdwn, joox, tiktok, twdwn, styletext, ringtone, quotesAnime, wallpaper, wikimedia, musically, pinterest, dBinary, eBinary, jadwaltv } = require("./../lib/dwn"); //fuck siapa yang ngubah oi?
 
 errer = {
     notparam: {
